a4da85eb1d3fa89643b201a8c43c4f5bcdb6690b
Jeff-HOU <1085363891@qq.com>
2019-04-15 20:33:10 +0800

    Updates

---
+++ precache-manifest.466b02c89517a65591e6096667284e9e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "289fb22ffe4151cb5628",
+    "url": "/WECOOP/static/css/main.b8bef114.chunk.css"
+  },
+  {
+    "revision": "289fb22ffe4151cb5628",
+    "url": "/WECOOP/static/js/main.1a008074.chunk.js"
+  },
+  {
+    "revision": "311267bae6763f30a2d6",
+    "url": "/WECOOP/static/js/runtime~main.03b113ac.js"
+  },
+  {
+    "revision": "9b24fde81dea1655673d",
+    "url": "/WECOOP/static/css/2.769d571d.chunk.css"
+  },
+  {
+    "revision": "9b24fde81dea1655673d",
+    "url": "/WECOOP/static/js/2.dce24c7c.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/WECOOP/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/WECOOP/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/WECOOP/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/WECOOP/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/WECOOP/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "04c632c5ea1d99d14543fb41f7ee14c7",
+    "url": "/WECOOP/index.html"
+  }
+];
