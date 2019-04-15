cc5a1e19354d7213b744aebfa8e64ac991d645bc
Jeff-HOU <1085363891@qq.com>
2019-04-15 21:06:40 +0800

    all work

---
+++ src/shared/pricing.js
@@ -0,0 +1,38 @@
+export const PRICING = [
+  {
+    "id": 0,
+    "name": "Basic",
+    "price": "2.99",
+    "features": [
+    "cheap",
+    "good",
+    "free"
+    ]
+  },
+  {
+    "id": 1,
+    "name": "Advanced",
+    "price": "9.99",
+    "features": [
+    "cheap2",
+    "good2",
+    "free2",
+    "cheap23",
+    "good23",
+    "free23"
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Professional",
+    "price": "19.99",
+    "features": [
+    "cheap24",
+    "good24",
+    "free24",
+    "cheap234",
+    "good234",
+    "free234"
+    ]
+  }
+];
