aac8ef26d12084afae54052e90cd73566a2f3c59
侯恺俊 HOU Kaijun, Jeff <44482228+webdev-jeff@users.noreply.github.com>
2019-04-24 22:31:33 +0800

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,16 @@
+// This helper file helpes github recognize the language this project uses as my code are put under `src` branch. Contents below are meanlingless.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+document.body.style = 'background: black;';
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
